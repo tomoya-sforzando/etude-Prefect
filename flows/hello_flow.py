3d121e483c880895d506f083a85b305ddc627ffc
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2021-03-25 16:32:42 +0900

    feat: :+1:  use LocalDaskExecutor

--- flows/hello_flow.py
+++ flows/hello_flow.py
@@ -1,13 +1,23 @@
+#!/usr/bin/env python3
+import os
+
 import prefect
-from prefect import task, Flow
+from prefect import task, Flow, Parameter
+from prefect.executors import LocalDaskExecutor
+from prefect.run_configs import LocalRun
+
 
 @task
-def say_hello():
+def say_hello(name):
+    greeting = os.environ.get('GREETING')
     logger = prefect.context.get('logger')
-    logger.info('Hello!')
+    logger.info(f'{greeting}, {name}!')
+
 
 with Flow('hello-flow') as flow:
-    say_hello()
+    people = Parameter('people', default=['Alice', 'Bob'])
+    say_hello.map(people)
 
-
+flow.run_config = LocalRun(env={'GREETING': 'Good bye'})
+flow.executor = LocalDaskExecutor()
 flow.register(project_name='etude')
