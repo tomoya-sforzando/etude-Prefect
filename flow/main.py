2eea8840dfc19d44e416d81769f7cbf3023171d6
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2022-02-17 20:55:12 +0900

    wip: :construction:  add parameter task

--- flow/main.py
+++ flow/main.py
@@ -1,7 +1,9 @@
 import os
+from datetime import datetime
 from typing import List
 
-from prefect import Client, Flow, Task
+import prefect
+from prefect import Client, Flow, Task, Parameter
 from prefect.executors import LocalDaskExecutor
 from prefect.run_configs import UniversalRun
 from prefect.storage import Local
@@ -11,7 +13,14 @@ PROJECT_NAME = os.getenv('PREFECT_PROJECT_NAME', 'etude-Prefect')
 class AbstractFlow:
     def __init__(self, client: Client, flow_name: str = "no_named_flow", e_tasks: List[Task] = [], t_tasks: List[Task] = [], l_tasks: List[Task] = []) -> None:
         self.client = client
+        self.flow_name = flow_name
         self.flow = Flow(name=flow_name, storage=Local(add_default_labels=False), executor=LocalDaskExecutor())
+
+        self.param = Parameter('msg', default='this is parameter')
+        self.datetime_param = prefect.core.parameter.DateTimeParameter('from_date', required=False)
+        self.flow.add_task(self.param)
+        self.flow.add_task(self.datetime_param)
+
         self.e_tasks = e_tasks
         self.t_tasks = t_tasks
         self.l_tasks = l_tasks
@@ -24,7 +33,7 @@ class AbstractFlow:
         self.flow.run_config = UniversalRun()
         flow_id = self.flow.register(project_name=PROJECT_NAME)
 
-        self.client.create_flow_run(flow_id=flow_id)
+        self.client.create_flow_run(flow_id=flow_id) #, parameters={'msg': "run from AbstractFlow", 'from_date': "2022-02-17T20:13:00+09:00"})
 
     def extract(self):
         for e_task in self.e_tasks:
@@ -43,8 +52,11 @@ class SayHelloTask(Task):
         super().__init__(**kwargs)
         self.module_name = 'say_hello_task'
 
-    def run(self):
-        self.logger.info('Hello World!')
+
+    def run(self, from_date: datetime = None, **kwargs):
+        flow_params = prefect.context.get("parameters", {})
+
+        self.logger.info(f'Hello World! {flow_params=} {type(from_date)=} {from_date=}')
 
 # Setup client
 client = Client()
@@ -52,6 +64,7 @@ client.create_project(project_name=PROJECT_NAME)
 
 # Setup flow
 basicFlow = AbstractFlow(client=client, flow_name="basicFlow", e_tasks=[SayHelloTask()], t_tasks=[], l_tasks=[])
+
 
 # Run flow
 basicFlow.run()
