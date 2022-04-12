78811fd755d34f83a0e18dec4dce79e4b9fd1cec
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2022-04-12 20:21:03 +0900

    perf: :zap:  rewrite task dependencies

--- flow/flows/idetail/idetail_flow.py
+++ flow/flows/idetail/idetail_flow.py
@@ -1,7 +1,7 @@
 import os
 from typing import List
 
-from prefect import flatten
+from prefect import flatten, unmapped
 
 from flows.abstract_flow_on_demand import AbstractFlowOnDemand
 from flows.idetail.idetail_settings import IdetailDemands, IdetailTasks
@@ -17,44 +17,76 @@ class IdetailFlow(AbstractFlowOnDemand):
         super().build(demands)
 
     def build_basic_flow(self):
-        self.tasks.get_csv_resource_data_by_product_task.set_upstream(
+        self.tasks.get_csv_resource_data_by_product_task.set_dependencies(
+            mapped=True,
+            keyword_tasks={
+                "product_name": self.tasks.get_products_task
+            },
+            upstream_tasks=[
+                self.tasks.get_products_task
+            ],
             flow=self.basic_flow,
-            task=self.tasks.get_products_task, key="product_name", mapped=True)
+        )
 
-        self.tasks.get_csv_master_data_task.set_upstream(
+        self.tasks.get_csv_master_data_task.set_dependencies(
+            mapped=True,
+            keyword_tasks={
+                "resource_data": flatten(self.tasks.get_csv_resource_data_by_product_task),
+                "master_csv_path": unmapped(self.tasks.get_paths_of_master_csv_task)
+            },
+            upstream_tasks=[
+                self.tasks.get_csv_resource_data_by_product_task,
+                self.tasks.get_paths_of_master_csv_task
+            ],
             flow=self.basic_flow,
-            task=flatten(self.tasks.get_csv_resource_data_by_product_task), key="resource_data", mapped=True)
-        self.tasks.get_csv_master_data_task.set_upstream(
-            flow=self.basic_flow,
-            task=self.tasks.get_paths_of_master_csv_task, key="master_csv_path", mapped=False)
+        )
 
-        self.tasks.delete_contents_task.set_upstream(
-            flow=self.basic_flow,
-            task=flatten(self.tasks.get_csv_resource_data_by_product_task), key="resource_data", mapped=True)
+        self.tasks.delete_contents_task.set_dependencies(
+            mapped=True,
+            keyword_tasks={
+                "resource_data": flatten(self.tasks.get_csv_resource_data_by_product_task)
+            },
+            upstream_tasks=[
+                self.tasks.get_csv_resource_data_by_product_task
+            ],
+            flow=self.basic_flow
+        )
 
-        self.tasks.register_contents_task.set_upstream(
+        self.tasks.register_contents_task.set_dependencies(
+            mapped=True,
+            keyword_tasks={
+                "master_data": self.tasks.get_csv_master_data_task,
+                "resource_data": flatten(self.tasks.get_csv_resource_data_by_product_task)
+            },
+            upstream_tasks=[
+                self.tasks.delete_contents_task,
+                self.tasks.get_csv_master_data_task,
+                self.tasks.get_csv_resource_data_by_product_task
+            ],
             flow=self.basic_flow,
-            task=self.tasks.delete_contents_task)
-        self.tasks.register_contents_task.set_upstream(
-            flow=self.basic_flow,
-            task=self.tasks.get_csv_master_data_task, key="master_data", mapped=True)
-        self.tasks.register_contents_task.set_upstream(
-            flow=self.basic_flow,
-            task=flatten(self.tasks.get_csv_resource_data_by_product_task), key="resource_data", mapped=True)
+        )
 
-        self.tasks.update_resources_by_product_task.set_upstream(
+        self.tasks.update_resources_by_product_task.set_dependencies(
+            mapped=True,
+            keyword_tasks={
+                "product_name": self.tasks.get_products_task,
+                "resource_data": self.tasks.get_csv_resource_data_by_product_task
+            },
+            upstream_tasks=[
+                self.tasks.get_products_task,
+                self.tasks.get_csv_resource_data_by_product_task
+            ],
             flow=self.basic_flow,
-            task=self.tasks.get_products_task, key="product_name", mapped=True)
-        self.tasks.update_resources_by_product_task.set_upstream(
-            flow=self.basic_flow,
-            task=self.tasks.get_csv_resource_data_by_product_task, key="resource_data", mapped=True)
+        )
 
-        self.tasks.update_status_by_s3_raw_data_path_task.set_upstream(
+        self.tasks.update_status_by_s3_raw_data_path_task.set_dependencies(
+            mapped=True,
+            keyword_tasks={
+                "resource_data": self.tasks.get_csv_resource_data_by_product_task
+            },
+            upstream_tasks=[
+                self.tasks.register_contents_task,
+                self.tasks.update_resources_by_product_task
+            ],
             flow=self.basic_flow,
-            task=self.tasks.register_contents_task)
-        self.tasks.update_status_by_s3_raw_data_path_task.set_upstream(
-            flow=self.basic_flow,
-            task=self.tasks.update_resources_by_product_task)
-        self.tasks.update_status_by_s3_raw_data_path_task.set_upstream(
-            flow=self.basic_flow,
-            task=self.tasks.get_csv_resource_data_by_product_task, key="resource_data", mapped=True)
+        )
